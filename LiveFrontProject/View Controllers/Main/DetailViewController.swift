5795092b339ce196703535d3f06e4cf04d743391
Shannon <shannonappdeveloper@gmail.com>
2022-08-21 16:02:44 -0600

    Better markdown formatting

--- LiveFrontProject/View Controllers/Main/DetailViewController.swift
+++ LiveFrontProject/View Controllers/Main/DetailViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Shannon Draeker on 8/19/22.
 //
 
+import MarkdownView
 import UIKit
 
 class DetailViewController: BaseViewController {
@@ -25,11 +26,11 @@ class DetailViewController: BaseViewController {
         // Set the content label if there are no subsections
         // TODO: - somehow test this...?
         if rule?.subsections?.isEmpty ?? true, let markdownText = rule?.desc {
-            contentLabel.attributedText = SwiftyMarkdown(string: markdownText, maxImageWidth: view.width * 0.9).applyDefaultFormatting(color: .text).attributedString()
+            contentLabel.load(markdown: markdownText, enableImage: false, css: css)
         }
 
         // Show either the subsections or the content label
-        contentContainerView.setVisible(if: rule?.subsections?.isEmpty ?? true)
+        contentLabel.setVisible(if: rule?.subsections?.isEmpty ?? true)
         subsectionsTableView.setVisible(if: rule?.subsections?.isEmpty == false)
     }
 
@@ -43,8 +44,9 @@ class DetailViewController: BaseViewController {
     // MARK: - UI Elements
 
     /// The label with the content of the rule, if applicable
-    private let contentContainerView = UIScrollView()
-    private lazy var contentLabel = MarkdownTextView(frame: .zero, textContainer: nil)
+    private lazy var contentLabel = MarkdownView()
+    /// The css styling for the markdown
+    private let css = "body { color:#FFF4C7; }"
 
     /// The tableview listing the subsections, if applicable
     private lazy var subsectionsTableView = UITableView(delegate: self, dataSource: self, cellClass: SimpleTableViewCell.self, separator: .singleLine)
@@ -53,16 +55,13 @@ class DetailViewController: BaseViewController {
 
     /// Add all the subviews to the view
     override func addAllSubviews() {
-        contentContainerView.addSubview(contentLabel)
-
-        view.addSubviews(contentContainerView, subsectionsTableView)
+        view.addSubviews(contentLabel, subsectionsTableView)
     }
 
     /// Set up the constraints and lay out all the elements in the view
     override func setUpConstraints() {
         // The label with the content of the rule, if applicable
-        contentContainerView.anchorFill(view, safeArea: true)
-        contentLabel.anchor([.top, .bottom], to: contentContainerView, padding: [10, 10]).anchorCenterX(to: contentContainerView)
+        contentLabel.anchorFill(view, safeArea: true)
 
         // The tableview listing the subsections, if applicable
         subsectionsTableView.anchorTop(to: view, 4, safeArea: true).anchorBottom(to: view, 10, safeArea: true).anchorCenterX(to: view, 1)
